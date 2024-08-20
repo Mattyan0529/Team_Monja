44f9bfa1d8441ba18882702dc4ec0715360b9671
松本哲希 <pikapikatetuki@gmail.com>
2024-08-21 00:15:21 +0900

    プルプル亀ら

--- Team_Monja/Assets/Scripts/Scripts_MT/Player/CameraManager_MT.cs
+++ Team_Monja/Assets/Scripts/Scripts_MT/Player/CameraManager_MT.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class CameraManager_MT : MonoBehaviour
 {
@@ -9,20 +8,20 @@ public class CameraManager_MT : MonoBehaviour
     private Transform playerTransform;
 
     private float mouseSensitivity = 100.0f; // マウス感度
-    private bool _InWall = false;//カメラが壁の中に入っているか
+    private bool _InWall = false; // カメラが壁の中に入っているか
+    private Vector3 shakeOffset = Vector3.zero; // 揺れのオフセット
+
     private void Awake()
     {
         FindPlayer();
     }
+
     void Start()
     {
-
         playerCamera = Camera.main.GetComponent<Camera>();
 
         // カーソルをロックして画面中央に固定
         Cursor.lockState = CursorLockMode.Locked;
-
-
     }
 
     // Update is called once per frame
@@ -30,23 +29,29 @@ public class CameraManager_MT : MonoBehaviour
     {
         CameraMove();
         CameraTransparent();
+
+        //カメラ振動のサンプル
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    StartCoroutine(CameraShake(0.3f, 0.5f));
+        //}
     }
+
     private void LateUpdate()
     {
         PlayerFollowing();
     }
 
     /// <summary>
-    ///プレイヤーを取得
+    /// プレイヤーを取得
     /// </summary>
     public void FindPlayer()
     {
         playerObj = GameObject.FindWithTag("Player");
-        if(playerObj != null)
+        if (playerObj != null)
         {
             playerTransform = playerObj.transform;
         }
-
     }
 
     /// <summary>
@@ -54,15 +59,17 @@ public class CameraManager_MT : MonoBehaviour
     /// </summary>
     private void PlayerFollowing()
     {
-        if(playerObj != null)
+        if (playerObj != null)
         {
+            // プレイヤーに追従
             transform.position = playerTransform.position;
+            // 追従後、揺れのオフセットを適用
+            transform.position += shakeOffset;
         }
     }
 
-
     /// <summary>
-    ///マウスでカメラを動かす 
+    /// マウスでカメラを動かす
     /// </summary>
     private void CameraMove()
     {
@@ -70,9 +77,31 @@ public class CameraManager_MT : MonoBehaviour
         float mouseX = Input.GetAxis("Horizontal2") * mouseSensitivity * Time.deltaTime
          + Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         //カメラを回転
-        transform.Rotate(Vector3.up * mouseX,Space.World);
+        transform.Rotate(Vector3.up * mouseX, Space.World);
         //プレイヤーを移動
         playerObj.transform.Rotate(Vector3.up * mouseX, Space.World);
+    }
+
+    /// <summary>
+    /// カメラを揺らす
+    /// </summary>
+    /// <param name="duration">時間</param>
+    /// <param name="magnitude">強さ</param>
+    /// <returns></returns>
+    public IEnumerator CameraShake(float duration, float magnitude)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            // 揺れのオフセットを計算
+            shakeOffset = Random.insideUnitSphere * magnitude;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        // 揺れ終了後、オフセットをリセット
+        shakeOffset = Vector3.zero;
     }
 
     /// <summary>
@@ -89,7 +118,6 @@ public class CameraManager_MT : MonoBehaviour
             playerCamera.nearClipPlane = 0.03f; // 初期値
         }
     }
-
 
     private void OnTriggerEnter(Collider other)
     {
