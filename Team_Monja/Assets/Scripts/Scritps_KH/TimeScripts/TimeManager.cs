9fa5b635a7687ecec7683f2ce135881339566ef5
ひ <hibiki0923k@gmail.com>
2024-08-23 13:47:50 +0900

    初めて敵を倒したらテキストが出るようになったよ

--- Team_Monja/Assets/Scripts/Scritps_KH/TimeScripts/TimeManager.cs
+++ Team_Monja/Assets/Scripts/Scritps_KH/TimeScripts/TimeManager.cs
@@ -50,6 +50,8 @@ public class TimeManager : MonoBehaviour
         _elapsedTime += Time.deltaTime;
         _elapsedTimeTotalGame += Time.deltaTime;
 
+        Debug.Log(_elapsedTimeTotalGame);
+
         // ƒvƒŒƒCƒ„[‚ðˆø‚«‚¸‚éê‡
         if(_elapsedTimeTotalGame > _timeDragPlayer)
         {
