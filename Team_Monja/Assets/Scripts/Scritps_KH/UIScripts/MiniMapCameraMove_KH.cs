9c90f8b756f4f18470949f43a4b3bb365e524394
STD\s20237036 <20237036-matsumototetsuki@hcs.ac.jp>
2024-09-02 17:42:21 +0900

    わすれてた

--- Team_Monja/Assets/Scripts/Scritps_KH/UIScripts/MiniMapCameraMove_KH.cs
+++ Team_Monja/Assets/Scripts/Scritps_KH/UIScripts/MiniMapCameraMove_KH.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class MiniMapCameraMove_KH : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _residentScript = default;
-
-    private PlayerManager_KH _playerManager = default;
 
     private GameObject _player;
 
     void Start()
     {
-        _playerManager = _residentScript.GetComponent<PlayerManager_KH>();
     }
 
     void Update()
