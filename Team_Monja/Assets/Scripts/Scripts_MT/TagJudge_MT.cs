7103be871960a02b2da4c27af56d6ce9a7115e5c
ひ <hibiki0923k@gmail.com>
2024-09-03 17:37:24 +0900

    DragPlayerToBossのSetPlayerが呼び出されてなかったよ～ん

--- Team_Monja/Assets/Scripts/Scripts_MT/TagJudge_MT.cs
+++ Team_Monja/Assets/Scripts/Scripts_MT/TagJudge_MT.cs
@@ -88,6 +88,7 @@ public class TagJudge_MT : MonoBehaviour
         _MiniMapIcon.GetComponent<PlayerMinimapIconSet>().SetPlayer(_playerObj);
 
         _damonHand.GetComponent<DamonHandPos>().SetPlayer(_playerObj);
+        _damonHand.GetComponent<DragPlayerToBoss_KH>().SetPlayer(_playerObj);
 
         _atkBar.GetComponent<StatusBar_MT>().SetPlayer(_playerObj);
         _defBar.GetComponent<StatusBar_MT>().SetPlayer(_playerObj);
