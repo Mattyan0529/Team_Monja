28ff7e05cca7fa958f8704364849a641895e708e
まっちゃん <20237075-suzukimasamune@hcs.ac.jp>
2024-09-17 14:58:57 +0900

    ワイトとスケルトン追加したStageらしいよ知らんけど

--- Team_Monja/Assets/Scripts/Scripts_MT/ControllerVibration_MT.cs
+++ Team_Monja/Assets/Scripts/Scripts_MT/ControllerVibration_MT.cs
@@ -1,14 +1,37 @@
 using UnityEngine;
+using UnityEngine.UI; // Toggleのために追加
 using UnityEngine.InputSystem;
 
 public class ControllerVibration_MT : MonoBehaviour
 {
-    
+    [SerializeField] private Toggle _vibrationToggle; // UIのトグルをアサインするためのフィールド
+    private bool _isVibrationEnabled = true; // 振動のオンオフを管理するフラグ
+
+    private void Start()
+    {
+        if (_vibrationToggle != null)
+        {
+            // トグルの状態が変更されたときに呼び出されるメソッドを登録
+            _vibrationToggle.onValueChanged.AddListener(OnToggleValueChanged);
+            _isVibrationEnabled = _vibrationToggle.isOn; // トグルの初期状態をフラグに設定
+        }
+    }
+
+    // トグルの状態が変更されたときに呼ばれるメソッド
+    private void OnToggleValueChanged(bool isOn)
+    {
+        _isVibrationEnabled = isOn;
+
+        if (!_isVibrationEnabled)
+        {
+            StopVibration(); // 振動をオフにしたらすぐに振動を止める
+        }
+    }
 
     // コントローラーの振動を開始
     public void VibrateController(float lowFrequency, float highFrequency, float duration)
     {
-        if (Gamepad.current != null)
+        if (_isVibrationEnabled && Gamepad.current != null)
         {
             // コントローラーを振動させる
             Gamepad.current.SetMotorSpeeds(lowFrequency, highFrequency);
