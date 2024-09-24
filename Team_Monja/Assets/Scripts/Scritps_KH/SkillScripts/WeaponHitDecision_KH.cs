634e69d8c528d156c32c348f9b351f0dfeb3f36b
ひ <hibiki0923k@gmail.com>
2024-09-24 11:31:34 +0900

    無敵！無敵！無敵！無敵！無敵！

--- Team_Monja/Assets/Scripts/Scritps_KH/SkillScripts/WeaponHitDecision_KH.cs
+++ Team_Monja/Assets/Scripts/Scritps_KH/SkillScripts/WeaponHitDecision_KH.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class WeaponHitDecision_KH : MonoBehaviour
 {
     private IDamagable_KH _skillInterface = default;
+    private InvincibleManagementScript_KH _invincibleManagementScript = default;
     private bool _isBoss = false;
 
     void Awake()
@@ -17,8 +18,13 @@ public class WeaponHitDecision_KH : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!other.gameObject.CompareTag("Player") && !other.gameObject.CompareTag("Enemy") && !other.gameObject.CompareTag("Boss")) return;
+
         // ƒK[ƒh’†‚Å‚ ‚ê‚ÎUŒ‚–³Œø
         if (other.gameObject.GetComponent<PlayerGuard_KH>() && other.gameObject.GetComponent<PlayerGuard_KH>().IsGuard) return;
+
+        // ‹N‚«ã‚ª‚è‚â•ßH’†‚Ì‚½‚ß–³“G‚Å‚ ‚ê‚ÎUŒ‚–³Œø
+        if (other.gameObject.GetComponent<InvincibleManagementScript_KH>().IsInvincible) return;
 
         if(_isBoss && other.gameObject.CompareTag("Player"))
         {
