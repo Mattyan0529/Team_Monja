5c2d64e57d84359bc08677ef01a7b80b5c597e8c
STD\s20237036 <20237036-matsumototetsuki@hcs.ac.jp>
2024-09-25 13:28:32 +0900

    食べ乗り中に食べ乗りできなくした

--- Team_Monja/Assets/Scripts/Scripts_MT/Player/EatOrChangeController_MT.cs
+++ Team_Monja/Assets/Scripts/Scripts_MT/Player/EatOrChangeController_MT.cs
@@ -8,6 +8,7 @@ public class EatOrChangeController_MT : MonoBehaviour
     private ChangeCharacter_MT changeCharacter;
     private EnemyTriggerManager_MT enemyTriggerManager;
     private LockOn lockOn;
+    PlayerMove_MT  _playerMove;
 
     [SerializeField]
     private float _activationDuration = 0.5f;
@@ -16,6 +17,7 @@ public class EatOrChangeController_MT : MonoBehaviour
     private GameObject _player;
     void Start()
     {
+        _playerMove = FindFirstObjectByType<PlayerMove_MT>();
         eatEnemy = GetComponent<EatEnemy_MT>();
         changeCharacter = GetComponent<ChangeCharacter_MT>();
         enemyTriggerManager = GameObject.FindWithTag("NearTrigger").GetComponent<EnemyTriggerManager_MT>();
@@ -25,18 +27,23 @@ public class EatOrChangeController_MT : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("eat"))
+        //食べたり乗り移るアニメーションちゅうは受け付けない
+        if (_playerMove.isActiveAndEnabled)
         {
-           
-            StartCoroutine(DeadManDelay(_activationDuration));
-            //RemoveClosestObject();
-        }
+            if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("eat"))
+            {
 
-        if (Input.GetKeyDown(KeyCode.Q) || Input.GetButtonDown("change"))
-        {
-            ChangeTagClosestObject();
-            lockOn.ResetRotation();
+                StartCoroutine(DeadManDelay(_activationDuration));
+                //RemoveClosestObject();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q) || Input.GetButtonDown("change"))
+            {
+                ChangeTagClosestObject();
+                lockOn.ResetRotation();
+            }
         }
+        
     }
 
     private IEnumerator DeadManDelay(float delay)
@@ -83,7 +90,6 @@ public class EatOrChangeController_MT : MonoBehaviour
     public void SetPlayer(GameObject player)
     {
         _player = player;
-
     }
 
 }
